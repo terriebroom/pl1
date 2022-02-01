812d6aeae82b9fecc346da5e98e205a61fc97aec
terriebroom <92108242+terriebroom@users.noreply.github.com>
2022-01-31 20:52:15 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
-    center: { lat: 44.039956, lng: -77.056496},
+    center: { lat: 44.039956, lng: -77.056496 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
